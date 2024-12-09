db8d19975006bfbd595b51c8d42eb7b4b9d7405d
Morpex_LT <alexandrmorpex@gmail.com>
2024-12-09 18:33:19 +0200

    Перед коррекцией aiohttp

--- try_to_request_all.py
+++ try_to_request_all.py
@@ -9,9 +9,9 @@ async def fetch_prices_binance():
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.json()  # Асинхронное получение JSON
-            price_list = {res['symbol']: float(res['bidPrice'])
+            price_list = {res['symbol']: float(res['askPrice'])
                           for res in data
-                          if res['symbol'].endswith('USDT') and res['bidPrice'] is not None and float(res['bidPrice']) != 0
+                          if res['symbol'].endswith('USDT') and res['askPrice'] is not None and float(res['askPrice']) != 0
                           }
             price_list = dict(price_list.items())
     return price_list
@@ -51,7 +51,7 @@ async def fetch_prices_bybit():
             price_list = {
                 res["symbol"]: float(res['ask1Price'])
                 for res in data["result"]['list']
-                if res["symbol"].endswith('USDT')}
+                if res["symbol"].endswith('USDT') and res['ask1Price'] is not None and float(res['ask1Price']) != 0}
             price_list = dict(sorted(price_list.items()))
             return price_list
 
@@ -64,7 +64,7 @@ async def fetch_prices_huobi():
             price_list = {
                 res["symbol"].upper(): float(res['ask'])
                 for res in data["data"]
-                if res["symbol"].upper().endswith('USDT')}
+                if res["symbol"].upper().endswith('USDT') and res['ask'] is not None and float(res['ask']) != 0}
             price_list = dict(sorted(price_list.items()))
             return price_list
 
@@ -79,7 +79,7 @@ async def fetch_prices_gateio():
             price_list = {
                 res["currency_pair"].replace('_', ''): float(res['lowest_ask'])
                 for res in data
-                if res["currency_pair"].replace('_', '').endswith('USDT')}
+                if res["currency_pair"].replace('_', '').endswith('USDT') and res['lowest_ask'] is not None and float(res['lowest_ask']) != 0}
             price_list = dict(sorted(price_list.items()))
             return price_list
 
@@ -92,7 +92,7 @@ async def fetch_prices_poloniex():
             price_list = {
                 res["symbol"].replace('_', ''): float(res['ask'])
                 for res in data
-                if res["symbol"].replace('_', '').endswith('USDT')}
+                if res["symbol"].replace('_', '').endswith('USDT') and res['ask'] is not None and float(res['ask']) != 0}
             price_list = dict(sorted(price_list.items()))
             return price_list
 
@@ -105,7 +105,7 @@ async def fetch_prices_bitget():
             price_list = {
                 res["symbol"]: float(res['buyOne'])
                 for res in data['data']
-                if res["symbol"].endswith('USDT')}
+                if res["symbol"].endswith('USDT') and res['buyOne'] is not None and float(res['buyOne']) != 0}
             price_list = dict(sorted(price_list.items()))
             return price_list
 
@@ -132,76 +132,79 @@ async def fetch_prices_ascendex():
             price_list = {
                 res["symbol"].replace('/', ''): float(res['ask'][0])
                 for res in data['data']
-                if res["symbol"].replace('/', '').endswith('USDT')}
+                if res["symbol"].replace('/', '').endswith('USDT') and res['ask'][0] is not None and float(res['ask'][0]) != 0}
             price_list = dict((price_list.items()))
             return price_list
 
 
-# Сравнение цен
-
-
-async def compare_prices():
-    # Собираем цены с разных бирж
-    binance_prices = await fetch_prices_binance()
-    kucoin_prices = await fetch_prices_kucoin()
-    okx_prices = await fetch_prices_okx()
-    bybit_prices = await fetch_prices_bybit()
-    huobi_prices = await fetch_prices_huobi()
-    poloniex_prices = await fetch_prices_poloniex()
-    bitget_prices = await fetch_prices_bitget()
-    ascendex_prices = await fetch_prices_ascendex()
-
-
 #################################################################
-all_prices = {}
 
-binance_response = asyncio.run(fetch_prices_binance())  # res1.keys()
-print('Binance: ', len(binance_response))
-all_prices['Binance'] = binance_response
+async def print_exchanges_info() -> dict:
+    all_prices = {}
 
-kucoiun_response = asyncio.run(fetch_prices_kucoin())
-print('Kucoin: ', len(kucoiun_response))
-all_prices['Kucoin'] = kucoiun_response
+    binance_response = await fetch_prices_binance()  # res1.keys()
+    print('Binance: ', len(binance_response))
+    all_prices['Binance'] = binance_response
 
-bybit_response = asyncio.run(fetch_prices_bybit())
-print('Bybit: ', len(bybit_response))
-all_prices['Bybit'] = bybit_response
+    kucoiun_response = await fetch_prices_kucoin()
+    print('Kucoin: ', len(kucoiun_response))
+    all_prices['Kucoin'] = kucoiun_response
 
-poloniex_huobi = asyncio.run(fetch_prices_huobi())
-print('Huobi: ', len(poloniex_huobi))
-all_prices['Huobi'] = poloniex_huobi
+    bybit_response = await fetch_prices_bybit()
+    print('Bybit: ', len(bybit_response))
+    all_prices['Bybit'] = bybit_response
 
-poloniex_response = asyncio.run(fetch_prices_poloniex())
-print('Poloniex: ', len(poloniex_response))
-all_prices['Poloniex'] = poloniex_response
+    poloniex_huobi = await fetch_prices_huobi()
+    print('Huobi: ', len(poloniex_huobi))
+    all_prices['Huobi'] = poloniex_huobi
 
-bitget_response = asyncio.run(fetch_prices_bitget())
-print('Bitget: ', len(bitget_response))
-all_prices['Bitget'] = bitget_response
+    poloniex_response = await fetch_prices_poloniex()
+    print('Poloniex: ', len(poloniex_response))
+    all_prices['Poloniex'] = poloniex_response
 
-ascendex_response = asyncio.run(fetch_prices_ascendex())
-print('Ascendex: ', len(ascendex_response))
-all_prices['Ascendex'] = ascendex_response
+    bitget_response = await fetch_prices_bitget()
+    print('Bitget: ', len(bitget_response))
+    all_prices['Bitget'] = bitget_response
 
-probit_response = asyncio.run(fetch_prices_probit())
-print('Probit: ', len(probit_response))
-all_prices['Probit'] = probit_response
+    ascendex_response = await fetch_prices_ascendex()
+    print('Ascendex: ', len(ascendex_response))
+    all_prices['Ascendex'] = ascendex_response
 
-okx_response = asyncio.run(fetch_prices_okx())
-print('Okx: ', len(okx_response))
-all_prices['Okx'] = okx_response
+    probit_response = await fetch_prices_probit()
+    print('Probit: ', len(probit_response))
+    all_prices['Probit'] = probit_response
+
+    okx_response = await fetch_prices_okx()
+    print('Okx: ', len(okx_response))
+    all_prices['Okx'] = okx_response
+
+    return all_prices
 
 
-async def sort_prices():
-    exchanges_list = all_prices.keys()
+async def sort_prices(prices_dict: dict):
+    exchanges_list = prices_dict.keys()
     sorted_dict = {}
     for exchange in exchanges_list:
-        for pair in all_prices[exchange]:
+        for pair in prices_dict[exchange]:
             if pair not in sorted_dict:
                 sorted_dict[pair] = []
-            sorted_dict[pair].append({exchange: all_prices[exchange][pair]})
+            sorted_dict[pair].append({exchange: prices_dict[exchange][pair]})
 
-    print(sorted_dict)
-    # return sorted_dict['BTCUSDT']
+    list_to_remove = []
+    for pair in sorted_dict.keys():
+        if len(sorted_dict[pair]) < 2:
+            list_to_remove.append(pair)
 
-asyncio.run(sort_prices())
+    for pair in list_to_remove:
+        sorted_dict.pop(pair)
+
+    for pair in sorted_dict:
+        print(pair, ': ',
+              len(sorted_dict[pair]),
+              )
+        for el in sorted_dict[pair]:
+            print(el, end='')
+        print('\n')
+
+info = asyncio.run(print_exchanges_info())
+asyncio.run(sort_prices(info))
