ad7478ca065eaa52737e19e691721ac04b604b7a
Morpex_LT <alexandrmorpex@gmail.com>
2024-12-09 19:10:16 +0200

    После коррекции асинхронности

--- try_to_request_all.py
+++ try_to_request_all.py
@@ -4,179 +4,153 @@ import pandas as pd
 import numpy as np
 
 
-async def fetch_prices_binance():
+async def fetch_prices_binance(session: aiohttp.ClientSession):
     url = 'https://api.binance.com/api/v3/ticker/24hr'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()  # Асинхронное получение JSON
-            price_list = {res['symbol']: float(res['askPrice'])
-                          for res in data
-                          if res['symbol'].endswith('USDT') and res['askPrice'] is not None and float(res['askPrice']) != 0
-                          }
-            price_list = dict(price_list.items())
-    return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res['symbol']: float(res['askPrice'])
+            for res in data
+            if res['symbol'].endswith('USDT') and res['askPrice'] is not None and float(res['askPrice']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_kucoin():
+async def fetch_prices_kucoin(session: aiohttp.ClientSession):
     url = 'https://api.kucoin.com/api/v1/market/allTickers'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res['symbol'].replace('-', ''): float(res['buy'])
-                for res in data['data']['ticker']
-                if res['symbol'].replace('-', '').endswith('USDT') and res['buy'] is not None and float(res['buy']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res['symbol'].replace('-', ''): float(res['buy'])
+            for res in data['data']['ticker']
+            if res['symbol'].replace('-', '').endswith('USDT') and res['buy'] is not None and float(res['buy']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_okx():
+async def fetch_prices_okx(session: aiohttp.ClientSession):
     url = 'https://www.okx.com/api/v5/market/tickers?instType=SPOT'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res['instId'].replace('-', ''): float(res['askPx'])
-                for res in data['data']
-                if res['instId'].replace('-', '').endswith('USDT') and res['askPx'] is not None and float(res['askPx']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res['instId'].replace('-', ''): float(res['askPx'])
+            for res in data['data']
+            if res['instId'].replace('-', '').endswith('USDT') and res['askPx'] is not None and float(res['askPx']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_bybit():
+async def fetch_prices_bybit(session: aiohttp.ClientSession):
     url = 'https://api.bybit.com/v5/market/tickers?category=spot'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["symbol"]: float(res['ask1Price'])
-                for res in data["result"]['list']
-                if res["symbol"].endswith('USDT') and res['ask1Price'] is not None and float(res['ask1Price']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["symbol"]: float(res['ask1Price'])
+            for res in data["result"]['list']
+            if res["symbol"].endswith('USDT') and res['ask1Price'] is not None and float(res['ask1Price']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_huobi():
+async def fetch_prices_huobi(session: aiohttp.ClientSession):
     url = 'https://api.huobi.pro/market/tickers'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["symbol"].upper(): float(res['ask'])
-                for res in data["data"]
-                if res["symbol"].upper().endswith('USDT') and res['ask'] is not None and float(res['ask']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
-
-# есть пустые значения
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["symbol"].upper(): float(res['ask'])
+            for res in data["data"]
+            if res["symbol"].upper().endswith('USDT') and res['ask'] is not None and float(res['ask']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_gateio():
+async def fetch_prices_gateio(session: aiohttp.ClientSession):
     url = 'https://api.gateio.ws/api/v4/spot/tickers'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["currency_pair"].replace('_', ''): float(res['lowest_ask'])
-                for res in data
-                if res["currency_pair"].replace('_', '').endswith('USDT') and res['lowest_ask'] is not None and float(res['lowest_ask']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["currency_pair"].replace('_', ''): float(res['lowest_ask'])
+            for res in data
+            if res["currency_pair"].replace('_', '').endswith('USDT') and res['lowest_ask'] is not None and float(res['lowest_ask']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_poloniex():
+async def fetch_prices_poloniex(session: aiohttp.ClientSession):
     url = 'https://api.poloniex.com/markets/ticker24h'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["symbol"].replace('_', ''): float(res['ask'])
-                for res in data
-                if res["symbol"].replace('_', '').endswith('USDT') and res['ask'] is not None and float(res['ask']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["symbol"].replace('_', ''): float(res['ask'])
+            for res in data
+            if res["symbol"].replace('_', '').endswith('USDT') and res['ask'] is not None and float(res['ask']) != 0
+        }
+        return price_list
 
 
-async def fetch_prices_bitget():
+async def fetch_prices_bitget(session: aiohttp.ClientSession):
     url = 'https://api.bitget.com/api/spot/v1/market/tickers'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["symbol"]: float(res['buyOne'])
-                for res in data['data']
-                if res["symbol"].endswith('USDT') and res['buyOne'] is not None and float(res['buyOne']) != 0}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
-
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["symbol"]: float(res['buyOne'])
+            for res in data['data']
+            if res["symbol"].endswith('USDT') and res['buyOne'] is not None and float(res['buyOne']) != 0
+        }
+        return price_list
 
 # ?? Хз че за биржа, долгий ответ, тупые ключи
-async def fetch_prices_probit():
+
+
+async def fetch_prices_probit(session: aiohttp.ClientSession):
     url = 'https://api.probit.com/api/exchange/v1/ticker'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["market_id"].replace('-', ''): float(res['last'])
-                for res in data['data']
-                if res["market_id"].replace('-', '').endswith('USDT') and res['last'] is not None}
-            price_list = dict(sorted(price_list.items()))
-            return price_list
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["market_id"].replace('-', ''): float(res['last'])
+            for res in data['data']
+            if res["market_id"].replace('-', '').endswith('USDT') and res['last'] is not None
+        }
+        return price_list
 
 
-async def fetch_prices_ascendex():
+async def fetch_prices_ascendex(session: aiohttp.ClientSession):
     url = 'https://ascendex.com/api/pro/v1/ticker'
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-            price_list = {
-                res["symbol"].replace('/', ''): float(res['ask'][0])
-                for res in data['data']
-                if res["symbol"].replace('/', '').endswith('USDT') and res['ask'][0] is not None and float(res['ask'][0]) != 0}
-            price_list = dict((price_list.items()))
-            return price_list
-
+    async with session.get(url) as response:
+        data = await response.json()
+        price_list = {
+            res["symbol"].replace('/', ''): float(res['ask'][0])
+            for res in data['data']
+            if res["symbol"].replace('/', '').endswith('USDT') and res['ask'][0] is not None and float(res['ask'][0]) != 0
+        }
+        return price_list
 
 #################################################################
 
+
 async def print_exchanges_info() -> dict:
-    all_prices = {}
+    async with aiohttp.ClientSession() as session:
+        tasks = {
+            'Binance': fetch_prices_binance(session),
+            'Kucoin': fetch_prices_kucoin(session),
+            'Bybit': fetch_prices_bybit(session),
+            'Huobi': fetch_prices_huobi(session),
+            'Poloniex': fetch_prices_poloniex(session),
+            'Bitget': fetch_prices_bitget(session),
+            'Ascendex': fetch_prices_ascendex(session),
+            'Probit': fetch_prices_probit(session),
+            'Okx': fetch_prices_okx(session),
+        }
 
-    binance_response = await fetch_prices_binance()  # res1.keys()
-    print('Binance: ', len(binance_response))
-    all_prices['Binance'] = binance_response
+        results = await asyncio.gather(*tasks.values(), return_exceptions=True)
+        all_prices = {}
 
-    kucoiun_response = await fetch_prices_kucoin()
-    print('Kucoin: ', len(kucoiun_response))
-    all_prices['Kucoin'] = kucoiun_response
-
-    bybit_response = await fetch_prices_bybit()
-    print('Bybit: ', len(bybit_response))
-    all_prices['Bybit'] = bybit_response
-
-    poloniex_huobi = await fetch_prices_huobi()
-    print('Huobi: ', len(poloniex_huobi))
-    all_prices['Huobi'] = poloniex_huobi
-
-    poloniex_response = await fetch_prices_poloniex()
-    print('Poloniex: ', len(poloniex_response))
-    all_prices['Poloniex'] = poloniex_response
-
-    bitget_response = await fetch_prices_bitget()
-    print('Bitget: ', len(bitget_response))
-    all_prices['Bitget'] = bitget_response
-
-    ascendex_response = await fetch_prices_ascendex()
-    print('Ascendex: ', len(ascendex_response))
-    all_prices['Ascendex'] = ascendex_response
-
-    probit_response = await fetch_prices_probit()
-    print('Probit: ', len(probit_response))
-    all_prices['Probit'] = probit_response
-
-    okx_response = await fetch_prices_okx()
-    print('Okx: ', len(okx_response))
-    all_prices['Okx'] = okx_response
+        for exchange, result in zip(tasks.keys(), results):
+            if isinstance(result, Exception):
+                print(f"Ошибка при запросе к {exchange}: {result}")
+            else:
+                print(f"{exchange}: {len(result)} пар")
+                all_prices[exchange] = result
 
     return all_prices
 
